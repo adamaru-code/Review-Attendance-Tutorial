f26497c88e1b7a9ccb4ab98595f319fa20af7f0f
adamaru <adamaru8700@icloud.com>
2022-05-19 09:37:03 +0000

    Finish full-fledged-login（7章終了 7.3チェックボックス追加）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user # log_in(user)略形
-      remember user # rememberヘルパーメソッド app/models/user.rb参照
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user) # (チェックボックス実装後) 
+      # remember user   #rememberヘルパーメソッド app/models/user.rb参照(チェックボックス実装前) 
       redirect_to user # redirect_to(user)略形
     else
       flash.now[:danger] ="認証に失敗しました。"
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
       else
         BCrypt::Engine.cost
       end
-  　BCrypt::Password.create(string, cost: cost)
+    BCrypt::Password.create(string, cost: cost)
   end
   
   # ランダムなトークンを返します。
@@ -37,7 +37,7 @@ class User < ApplicationRecord
   # トークンがダイジェストと一致すればtrueを返します。
   def authenticated?(remember_token)
     # ダイジェストが存在しない場合はfalseを返して終了します。
-    return false if remember_digest.nil?    
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
