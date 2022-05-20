1b162c2ec81d396cabb2eb5b9f0471eee90fb873
adamaru <adamaru8700@icloud.com>
2022-05-20 04:49:36 +0000

    8章8.3 フレンドリーフォワーディング機能追加 （store_location と redirect_back_orという2つのヘルパーメソッド）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in user # log_in(user)略形
       params[:session][:remember_me] == '1' ? remember(user) : forget(user) # (チェックボックス実装後) 
       # remember user   #rememberヘルパーメソッド app/models/user.rb参照(チェックボックス実装前) 
-      redirect_to user # redirect_to(user)略形
+      redirect_back_or user # フレンドリーフォワーディング (変更前 redirect_to user ← redirect_to(user)略形)
     else
       flash.now[:danger] ="認証に失敗しました。"
       render :new
